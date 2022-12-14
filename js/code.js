93aac24025c4e72f6e19a2e03d40725ac8351766
adriankr38 <adrian58ago@gmail.com>
2022-12-14 17:40:38 +0100

    add(Classes) : class `Shop` and `ShopItem`

--- js/code.js
+++ js/code.js
@@ -1,4 +1,4 @@
-import items from "../data/items.json" assert {type:"json"}; 
+import items from "../data/items.json" assert { type: "json" };
 
 let suits = document.getElementById("suits");
 let ties = document.getElementById("ties");
@@ -6,6 +6,15 @@ let accs = document.getElementById("accesories");
 let suitsStyle = window.getComputedStyle(suits);
 let tiesStyle = window.getComputedStyle(ties);
 let accsStyle = window.getComputedStyle(accs);
+
+const unCamelCase = function (word) {
+  return word
+    .split(/(?=[A-Z])/)
+    .map((p) => {
+      return p.charAt(0).toUpperCase() + p.slice(1);
+    })
+    .join(" ");
+};
 
 let backgroundDisplays = {
   black: {
@@ -80,6 +89,7 @@ function displayAcc() {
     ties.style.zIndex = "0";
   }
 }
+
 let options = Object.keys(backgroundDisplays);
 let currentIndex = 0;
 
@@ -90,8 +100,6 @@ function changeHero1Background(bgType) {
   let hero1 = document.getElementsByClassName("hero1")[0];
   let textBox = hero1.children[0];
   let bgConfig = {};
-  console.log(bgType);
-  console.log(currentIndex);
 
   switch (bgType) {
     case "black":
@@ -124,7 +132,6 @@ function changeHero1Background(bgType) {
   currentIndex = incrementIndex(currentIndex);
 }
 
-
 function incrementIndex(index) {
   if (currentIndex >= 2) {
     index = 0;
@@ -135,22 +142,161 @@ function incrementIndex(index) {
 }
 
 class Shop {
-  constructor(){
-    let this.items = []
+  constructor() {
+    this.cart = [];
+    this.cartHoverHtml = document.getElementsByClassName("shop-hover")[0];
+    this.cartItemCount = document.querySelector(".n-items");
+  }
+
+  renderItemCount(increment) {
+    if (increment) {
+      this.cartItemCount.textContent =
+        parseInt(this.cartItemCount.textContent) + 1 + "";
+    } else {
+      this.cartItemCount.textContent =
+        parseInt(this.cartItemCount.textContent) - 1 + "";
+    }
+  }
+
+  addToCart(ShopItem) {
+    if (!this.cart.length) {
+      ShopItem.count++;
+      this.cart.push(ShopItem);
+      this.renderItemCount(true);
+      this.buildHoverHtml();
+      return;
+    } else {
+      for (let el of this.cart) {
+        if (el.name == ShopItem.name) {
+          el.count++;
+          this.renderItemCount(true);
+          this.buildHoverHtml();
+          return;
+        }
+      }
+      ShopItem.count++;
+      this.cart.push(ShopItem);
+      this.renderItemCount(true);
+    }
+    this.buildHoverHtml();
+  }
+
+  removeFromCart(ShopItem) {
+    for (let [i, el] of this.cart.entries()) {
+      if (el.name == ShopItem.name) {
+        el.count--;
+        this.renderItemCount(false);
+        if (!el.count) {
+          this.cart.splice(i, 1);
+          let itemsHtml = document.querySelector(".shop-hover");
+          for (let child of itemsHtml.children) {
+            if (child.id === "li-" + ShopItem.name) {
+              console.log("removing");
+              itemsHtml.removeChild(child);
+            }
+          }
+        }
+      }
+    }
+    console.log(this.cart);
+    this.buildHoverHtml();
+  }
+  buildHoverHtml() {
+    if (!this.cart.length) {
+      this.cartHoverHtml.innerHTML =
+        "<li id='emptyCart'><p>No items on cart</p></li>";
+    } else {
+      let htmlString = "";
+      refreshIconEvents();
+      for (const el of this.cart) {
+        const htmlEl = document.querySelector(`#li-${el.name}`);
+        let hasListener = "";
+        if (htmlEl) {
+          hasListener = htmlEl.children[0].children[0].getAttribute("listener");
+        }
+
+        htmlString += `<li id = 'li-${
+          el.name
+        }' class='hover-item'><div class='pre-count'><i class='material-icons add-item' style=color:white listener = '${hasListener}'>expand_less</i><span>${unCamelCase(
+          el.name
+        )}</span><i class='material-icons subtract-item' style=color:white listener='${hasListener}'>expand_more</i></div><span id='item-count'>${
+          el.count
+        }</span></li><hr>`;
+      }
+      this.cartHoverHtml.innerHTML = htmlString;
+      refreshIconEvents();
+      return htmlString;
+    }
   }
 }
 
-class ShopItem{
-  constructor(name,price){
-    this.name = name
-    this.price = price
+class ShopItem {
+  constructor(name, price, imageDir) {
+    this.name = name;
+    this.price = price;
+    this.imageDir = imageDir;
+    this.count = 0;
   }
-
 }
 
-console.log(items)
+let shopItemsInfo = [];
+let myShop = new Shop();
+myShop.buildHoverHtml();
+
+for (const [k, v] of Object.entries(items)) {
+  shopItemsInfo.push(new ShopItem(k, v["price"], v["image"]));
+}
+
+for (let i = 0; i < shopItemsInfo.length; i++) {
+  let iterShopEl = shopItemsInfo[i];
+  let collection = document.getElementById(iterShopEl.name).children;
+  if (!isNaN(iterShopEl.price)) {
+    collection[1].innerHTML = "" + iterShopEl.price + "&euro;";
+  } else {
+    collection[1].innerHTML = "Contact";
+  }
+}
+let suitsButton = document.getElementById("displaySuits");
+let tiesButton = document.getElementById("displayTies");
+let accsButton = document.getElementById("displayAcc");
+
+suitsButton.addEventListener("click", displaySuits, false);
+tiesButton.addEventListener("click", displayTies, false);
+accsButton.addEventListener("click", displayAcc, false);
+
+function addItemToCartFromButtonClick(itemDict, shop, button) {
+  let id = button.parentNode.id;
+  let item = new ShopItem(id, itemDict[id]["price"], itemDict[id]["image"]);
+  shop.addToCart(item);
+}
+
+function shopItemModifyCountFromIconClick(itemDict, shop, i) {
+  let id = i.parentNode.parentNode.id.slice(3);
+  let item = new ShopItem(id, itemDict[id]["price"], itemDict[id]["image"]);
+  if (i.className.includes("add-item")) {
+    shop.addToCart(item);
+  } else {
+    shop.removeFromCart(item);
+  }
+}
+
+let addToShopButtons = document.getElementsByClassName("add-to-shop-button");
+for (const button of addToShopButtons) {
+  button.addEventListener("click", function () {
+    addItemToCartFromButtonClick(items, myShop, button);
+  });
+}
+
+const refreshIconEvents = function () {
+  let addOrDecreaseIcons = document.querySelectorAll(".material-icons");
+  for (const icon of addOrDecreaseIcons) {
+    icon.addEventListener("click", function () {
+      shopItemModifyCountFromIconClick(items, myShop, icon);
+    });
+    icon.setAttribute("listener", "true");
+  }
+};
 
 setInterval(function () {
-  changeHero1Background((bgType = options[currentIndex]));
-}, 60000);
-
+  changeHero1Background(options[currentIndex]);
+}, 30000);
